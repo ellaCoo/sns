2079ffa1f8cbf57290f4421f62e93edd2c014a10
jglorys <jglorys@naver.com>
2024-08-17 19:33:29 +0900

    #11 - more 버튼 클릭시 ajax로 data post방식으로 가져오기 구현
    - 타임리프 자체는 서버 사이드 템플릿 엔진으로, 클라이언트 사이드에서 Ajax 호출을 직접적으로 처리할 수는 없기에 script에 ajax호출 코드를 추가함.
    - 날짜 포맷팅을 위한 moment.js 를 추가 하였다
    - ajax post 호출시 403 에러가 발생하는 부분은 헤더에 값을 추가하여 해결함.
    *https://velog.io/@jyleedev/514-ajax-post-%ED%86%B5%EC%8B%A0-403-error

--- src/main/java/com/project/sns/controller/PostController.java
+++ src/main/java/com/project/sns/controller/PostController.java
@@ -4,14 +4,17 @@ import com.project.sns.dto.response.PostResponse;
 import com.project.sns.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/posts")
@@ -21,11 +24,16 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping
-    public String posts(
-            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-            ModelMap map) {
+    public String posts(ModelMap map) {
+        Pageable pageable = PageRequest.of(0, 10, Sort.Direction.DESC, "createdAt");
         Page<PostResponse> posts = postService.getPosts(pageable).map(PostResponse::fromDto);
         map.addAttribute("posts", posts);
         return "posts/index";
+    }
+    @PostMapping
+    @ResponseBody
+    public Page<PostResponse> getPosts(@RequestParam("page") int page) {
+        Pageable pageable = PageRequest.of(page, 10, Sort.Direction.DESC, "createdAt");
+        return postService.getPosts(pageable).map(PostResponse::fromDto);
     }
 }
