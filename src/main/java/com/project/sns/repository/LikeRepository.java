b9fa415b4213da7a375f4f260947f9cdb535e60d
jglorys <jglorys@naver.com>
2024-08-27 16:48:11 +0900

    #10 - Querydsl 상속 제거

--- src/main/java/com/project/sns/repository/LikeRepository.java
+++ src/main/java/com/project/sns/repository/LikeRepository.java
@@ -10,8 +10,6 @@ import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 
 public interface LikeRepository extends
-        JpaRepository<Like, Long>,
-        QuerydslPredicateExecutor<Like>, // entity 안에 있는 모든 필드에 대한 기본 검색 기능 추가
-        QuerydslBinderCustomizer<QLike>  // 부분 검색
+        JpaRepository<Like, Long>
 {
 }
--- src/main/java/com/project/sns/repository/PostHashtagRepository.java
+++ src/main/java/com/project/sns/repository/PostHashtagRepository.java
@@ -10,8 +10,6 @@ import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 
 public interface PostHashtagRepository extends
-        JpaRepository<PostHashtag, Long>,
-        QuerydslPredicateExecutor<PostHashtag>, // entity 안에 있는 모든 필드에 대한 기본 검색 기능 추가
-        QuerydslBinderCustomizer<QPostHashtag>  // 부분 검색
+        JpaRepository<PostHashtag, Long>
 {
 }
