6389f7e7f5afe5808b07315393f9afa3bf0a1fab
jglorys <jglorys@naver.com>
2024-08-30 15:10:35 +0900

    #7 - 포스트 생성 프론트,백엔드 구현

--- src/main/java/com/project/sns/controller/PostController.java
+++ src/main/java/com/project/sns/controller/PostController.java
@@ -1,6 +1,8 @@
 package com.project.sns.controller;
 
+import com.project.sns.domain.Post;
 import com.project.sns.domain.constant.FormStatus;
+import com.project.sns.dto.PostDto;
 import com.project.sns.dto.UserAccountDto;
 import com.project.sns.dto.request.PostRequest;
 import com.project.sns.dto.response.PostCommentDto;
@@ -94,5 +96,25 @@ public class PostController {
         postService.deletePost(postId, boardPrincipal.getUsername());
 
         return "redirect:/posts";
+    }
+
+    @GetMapping("/form")
+    public String createPostPage(ModelMap map) {
+        PostResponse post = PostResponse.of("");
+        map.addAttribute("post", post);
+        map.addAttribute("formStatus", FormStatus.CREATE);
+        return "posts/form";
+    }
+
+    @PostMapping("/form")
+    public String createPost(
+        @AuthenticationPrincipal BoardPrincipal boardPrincipal,
+        PostRequest postRequest,
+        ModelMap map) {
+            PostDto postDto = postService.createPost(postRequest.toDto(boardPrincipal.toDto()));
+            PostResponse post = PostResponse.fromDto(postDto);
+
+            map.addAttribute("post", post);
+            return "posts/detail";
     }
 }
