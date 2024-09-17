e3746d66cd7c800946ee3001183512514318b351
jglorys <jglorys@naver.com>
2024-09-18 01:57:50 +0900

    #51 - 사용자 인증 시, redis 캐시 사용할 수 있도록 개발

--- src/main/java/com/project/sns/service/UserAccountService.java
+++ src/main/java/com/project/sns/service/UserAccountService.java
@@ -2,6 +2,7 @@ package com.project.sns.service;
 
 import com.project.sns.domain.UserAccount;
 import com.project.sns.dto.UserAccountDto;
+import com.project.sns.repository.UserAccountCacheRepository;
 import com.project.sns.repository.UserAccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,11 +15,19 @@ import java.util.Optional;
 @Service
 public class UserAccountService {
     private final UserAccountRepository userAccountRepository;
+    private final UserAccountCacheRepository userAccountCacheRepository;
 
     @Transactional(readOnly = true)
     public Optional<UserAccountDto> searchUser(String username) {
-        return userAccountRepository.findById(username)
-                .map(UserAccountDto::fromEntity);
+        var userEntityCache = userAccountCacheRepository.getUserAccountCache(username);
+
+        if (userEntityCache.isPresent()) {
+            return userEntityCache.map(UserAccountDto::fromEntity);
+        } else {
+            Optional<UserAccount> userAccount = userAccountRepository.findById(username);
+            userAccountCacheRepository.setUserAccountCache(userAccount.get());
+            return userAccount.map(UserAccountDto::fromEntity);
+        }
     }
 
     public UserAccountDto saveUser(String username, String password, String email, String nickname, String memo) {
